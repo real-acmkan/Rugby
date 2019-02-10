98707a7512d1eccc1df7e8b492925e6ea77c8afd
ACMKAN <matthewt18@learn.cssd.ab.ca>
2019-02-10 13:04:26 -0700

    Rugby Backend

---
+++ Rugby_Backend/app.js
@@ -0,0 +1,32 @@
+function myFunction() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBTbdewBsyAH1VUgYpQqXZ2tkCqT49Rm0I",
+    authDomain: "rugbytest-6f0d9.firebaseapp.com",
+    databaseURL: "https://rugbytest-6f0d9.firebaseio.com",
+    projectId: "rugbytest-6f0d9",
+    storageBucket: "rugbytest-6f0d9.appspot.com",
+    messagingSenderId: "474217368223"
+  };
+  firebase.initializeApp(config);
+  var firestore = firebase.firestore();
+  // End of Firebase Initialization
+
+  const docRef = firestore.doc("samples/gameData");
+  const outputHeader = document.querySelector("#gameScore");
+  const inputTextField = document.querySelector("#lastestGameScore");
+  const saveButton = document.querySelector("#saveButton");
+
+  saveButton.addEventListener("click", function() {
+    const textToSave = inputTextField.value;
+    console.log("I am going to save " + textToSave + " to Firestore");
+    docRef.set({
+      gameScore: textToSave
+    }).then(function() {
+      console.log("Status saved!");
+      // DEBUGGING
+    }).catch(function (error) {
+      console.log("Got an error: ", error);
+    });
+  })
+}
