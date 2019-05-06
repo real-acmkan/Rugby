f85e2d8d0016fbb679eb3fa3eab58094ff48a45e
ACMKAN <matthewt18@learn.cssd.ab.ca>
2019-05-05 18:32:26 -0600

    Updated everything

--- Rugby_Backend/app.js
+++ Rugby_Backend/app.js
@@ -7,7 +7,8 @@ function myFunction() {
     databaseURL: "https://rugbytest-6f0d9.firebaseio.com",
     projectId: "rugbytest-6f0d9",
     storageBucket: "rugbytest-6f0d9.appspot.com",
-    messagingSenderId: "474217368223"
+    messagingSenderId: "474217368223",
+    appId: "1:474217368223:web:982346b400fb4aac"
   };
   if (!firebase.apps.length) {
     firebase.initializeApp(config);
