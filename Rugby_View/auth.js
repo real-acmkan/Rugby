345de79a92f284236d52ae5fe8545ea0c91ce8ea
ACMKAN <matthewt18@learn.cssd.ab.ca>
2019-05-05 21:04:50 -0600

    Updated auth

---
+++ Rugby_View/auth.js
@@ -0,0 +1,12 @@
+var config = {
+  apiKey: "AIzaSyBTbdewBsyAH1VUgYpQqXZ2tkCqT49Rm0I",
+  authDomain: "rugbytest-6f0d9.firebaseapp.com",
+  databaseURL: "https://rugbytest-6f0d9.firebaseio.com",
+  projectId: "rugbytest-6f0d9",
+  storageBucket: "rugbytest-6f0d9.appspot.com",
+  messagingSenderId: "474217368223",
+  appId: "1:474217368223:web:982346b400fb4aac"
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
